f9cba33050404546d1629ddebd1ce7e51f894613
maro <kamanbe0120@gmail.com>
2020-12-23 02:37:34 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
 import React from 'react';
+import liff from '@line/liff'; // 追加
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
+  /* 追加: メッセージ送信 */
+  const sendMessage = () => {
+    liff.init({liffId: process.env.REACT_APP_LIFF_ID as string}) // LIFF IDをセットする
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login({}) // ログインしていなければ最初にログインする
+        } else if (liff.isInClient()) { // LIFFので動いているのであれば
+          liff.sendMessages([{ // メッセージを送信する
+            'type': 'text',
+            'text': "You've successfully sent a message! Hooray!"
+          }]).then(function() {
+            window.alert('Message sent');
+          }).catch(function(error) {
+            window.alert('Error sending message: ' + error);
+          });
+        }
+      })
+  }
+
+  /* 追加: UserProfileをAlertで表示 */
+  const getUserInfo = () => {
+    liff.init({liffId: process.env.REACT_APP_LIFF_ID as string})
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login({}) // ログインしていなければ最初にログインする
+        } else if (liff.isInClient()) {
+          liff.getProfile()  // ユーザ情報を取得する
+            .then(profile => {
+              const userId: string = profile.userId
+              const displayName: string = profile.displayName
+              alert(`Name: ${displayName}, userId: ${userId}`)
+            }).catch(function(error) {
+              window.alert('Error sending message: ' + error);
+            });
+        }
+      })
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +51,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button className="button" onClick={sendMessage}>send message</button>
+        <button className="button" onClick={getUserInfo}>show user info</button>
         <a
           className="App-link"
           href="https://reactjs.org"
